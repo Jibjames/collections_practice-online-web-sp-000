f5727522cac9c945e35581c1bd22442b16ec1fa8
James Bridges <masterbridges@gmail.com>
2019-12-23 20:55:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -24,3 +24,12 @@ end
 def reverse_array(array)
   array.reverse
 end
+
+def kesha_maker(array)
+  
+  array.map do |name|
+    new_name = name.split
+    new_name[2] = $
+    new_name.join
+  end
+end
